a1c9e0ce22833f74bdca4dd7f21ea4b7623b7f3e
José Padilla <jpadilla@webapplicate.com>
2019-02-19 20:01:35 -0500

    Setup for deploying to heroku

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,14 @@
 const withCSS = require('@zeit/next-css');
 
+const PRODUCTION_API_URL = 'https://api.contratospr.com/v1';
+const DEVELOPMENT_API_URL = 'http://localhost:8000/v1';
+
 module.exports = withCSS({
   env: {
-    API_URL: 'http://localhost:8000/api/v1'
+    API_URL:
+      process.env.NODE_ENV === 'production'
+        ? PRODUCTION_API_URL
+        : DEVELOPMENT_API_URL
   },
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
