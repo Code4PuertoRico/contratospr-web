47b7dd5be4983c81ec76f949763fa9fe1c7220ec
José Padilla <jpadilla@webapplicate.com>
2019-02-20 23:47:35 -0500

    Convert ContractsChart into a functional component

--- src/components/contracts-chart.js
+++ src/components/contracts-chart.js
@@ -8,119 +8,103 @@ const DEFAULT_POINT_RADIUS = 3;
 const SELECTED_POINT_BG_COLOR = 'rgba(52, 144, 220, 1)';
 const SELECTED_POINT_RADIUS = 5;
 
-class ContractsChart extends React.Component {
-  constructor(props) {
-    super(props);
+function ContractsChart({ height, data }) {
+  let chartData = {
+    datasets: [
+      {
+        backgroundColor: 'rgba(52, 144, 220, 0.5)',
+        borderColor: 'rgba(52, 144, 220, 1)',
+        pointBackgroundColor: data.map(() => DEFAULT_POINT_BG_COLOR),
+        pointRadius: data.map(() => DEFAULT_POINT_RADIUS),
+        fill: true,
+        data: data
+      }
+    ]
+  };
 
-    this.state = {
-      height: props.height,
-      data: {
-        datasets: [
-          {
-            backgroundColor: 'rgba(52, 144, 220, 0.5)',
-            borderColor: 'rgba(52, 144, 220, 1)',
-            pointBackgroundColor: props.data.map(() => DEFAULT_POINT_BG_COLOR),
-            pointRadius: props.data.map(() => DEFAULT_POINT_RADIUS),
-            fill: true,
-            data: props.data
-          }
-        ]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        tooltips: {
-          displayColors: false,
-          callbacks: {
-            label: function(item, data) {
-              let contracts = data.datasets[0].data[item.index].contracts;
-              return [
-                'Contratos: ' + contracts,
-                'Total: $' + item.yLabel.toLocaleString()
-              ];
-            }
-          }
-        },
-        scales: {
-          xAxes: [
-            {
-              display: false,
-              type: 'time',
-              time: {
-                tooltipFormat: 'll'
-              }
-            }
-          ],
-          yAxes: [
-            {
-              display: true,
-              ticks: {
-                beginAtZero: true,
-                maxTicksLimit: 5,
-                callback: function(value) {
-                  return '$' + millify(value);
-                }
-              }
-            }
-          ]
-        },
-        onClick: function(e) {
-          let firstPoint = this.getElementAtEvent(e)[0];
-          let dataset = this.data.datasets[0];
-
-          let contractsElements = document
-            .getElementById('contracts-list')
-            .querySelectorAll('a');
-          let contracts = Array.from(contractsElements);
-
-          if (firstPoint) {
-            let value = dataset.data[firstPoint._index];
-
-            dataset.pointBackgroundColor = props.data.map(
-              () => DEFAULT_POINT_BG_COLOR
-            );
-            dataset.pointBackgroundColor[
-              firstPoint._index
-            ] = SELECTED_POINT_BG_COLOR;
-            dataset.pointRadius = props.data.map(() => DEFAULT_POINT_RADIUS);
-            dataset.pointRadius[firstPoint._index] = SELECTED_POINT_RADIUS;
-            this.update({ duration: 0 });
-
-            contracts.forEach(function(contract) {
-              if (value.x === contract.getAttribute('data-date')) {
-                contract.style.display = '';
-              } else {
-                contract.style.display = 'none';
-              }
-            });
-          } else {
-            dataset.pointBackgroundColor = props.data.map(
-              () => DEFAULT_POINT_BG_COLOR
-            );
-            dataset.pointRadius = props.data.map(() => DEFAULT_POINT_RADIUS);
-            this.update({ duration: 0 });
-
-            contracts.forEach(function(contract) {
-              contract.style.display = '';
-            });
-          }
+  let chartOptions = {
+    legend: {
+      display: false
+    },
+    tooltips: {
+      displayColors: false,
+      callbacks: {
+        label: function(item, data) {
+          let contracts = data.datasets[0].data[item.index].contracts;
+          return [
+            'Contratos: ' + contracts,
+            'Total: $' + item.yLabel.toLocaleString()
+          ];
         }
       }
-    };
-  }
+    },
+    scales: {
+      xAxes: [
+        {
+          display: false,
+          type: 'time',
+          time: {
+            tooltipFormat: 'll'
+          }
+        }
+      ],
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            callback: function(value) {
+              return '$' + millify(value);
+            }
+          }
+        }
+      ]
+    },
+    onClick: function(e) {
+      let firstPoint = this.getElementAtEvent(e)[0];
+      let dataset = this.data.datasets[0];
 
-  render() {
-    return (
-      <>
-        <Line
-          data={this.state.data}
-          height={this.state.height}
-          options={this.state.options}
-        />
-      </>
-    );
-  }
+      let contractsElements = document
+        .getElementById('contracts-list')
+        .querySelectorAll('a');
+      let contracts = Array.from(contractsElements);
+
+      if (firstPoint) {
+        let value = dataset.data[firstPoint._index];
+
+        dataset.pointBackgroundColor = data.map(() => DEFAULT_POINT_BG_COLOR);
+        dataset.pointBackgroundColor[
+          firstPoint._index
+        ] = SELECTED_POINT_BG_COLOR;
+        dataset.pointRadius = data.map(() => DEFAULT_POINT_RADIUS);
+        dataset.pointRadius[firstPoint._index] = SELECTED_POINT_RADIUS;
+        this.update({ duration: 0 });
+
+        contracts.forEach(function(contract) {
+          if (value.x === contract.getAttribute('data-date')) {
+            contract.style.display = '';
+          } else {
+            contract.style.display = 'none';
+          }
+        });
+      } else {
+        dataset.pointBackgroundColor = data.map(() => DEFAULT_POINT_BG_COLOR);
+        dataset.pointRadius = data.map(() => DEFAULT_POINT_RADIUS);
+        this.update({ duration: 0 });
+
+        contracts.forEach(function(contract) {
+          contract.style.display = '';
+        });
+      }
+    }
+  };
+
+  return (
+    <>
+      <Line data={chartData} height={height} options={chartOptions} />
+    </>
+  );
 }
 
 ContractsChart.propTypes = {
