b92b8fccfa06bcac1242a346cc06fadce78a73ee
José Padilla <jpadilla@webapplicate.com>
2019-02-21 23:46:39 -0500

    Add page loading indicator

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Router from 'next/router';
 import App, { Container } from 'next/app';
+import NProgress from 'nprogress';
 import Layout from '../layouts/main';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default class CustomApp extends App {
   static async getInitialProps({ Component, ctx }) {
