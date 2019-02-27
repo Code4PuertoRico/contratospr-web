15ef89cea7e8a364a665df112272e5ab284cfc47
José Padilla <jpadilla@webapplicate.com>
2019-02-27 17:37:50 -0500

    Render contract document with pdf.js (#7)
    * Fix eslint config
    
    * Format tsx files on save
    
    * Lint tsx staged files
    
    * Render contract document with pdf.js
    
    * Move viewer to pdfs.contratospr.com

---
+++ src/components/pdf-viewer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface PDFViewerProps {
+  src: string;
+}
+
+class PDFViewer extends React.Component<PDFViewerProps> {
+  render() {
+    let fileUrl = `/_api/document?url=${encodeURIComponent(this.props.src)}`;
+    let src = `/static/pdfjs/viewer.html?file=${encodeURIComponent(fileUrl)}`;
+
+    return (
+      <div>
+        <style jsx>{`
+          .pdf-viewer-container {
+            overflow: hidden;
+            padding-top: 100%;
+            position: relative;
+          }
+
+          .pdf-viewer-container iframe {
+            border: 0;
+            height: 100%;
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: 100%;
+          }
+        `}</style>
+        <div className="pdf-viewer-container">
+          <iframe src={src} frameBorder="0" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PDFViewer;
