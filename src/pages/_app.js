9f813bc9eb191e242801e538540e863e3de99f96
José Padilla <jpadilla@webapplicate.com>
2019-09-26 20:32:04 -0400

    Review deprecation errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Router from 'next/router';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import NProgress from 'nprogress';
 import Layout from '../layouts/main';
 
@@ -9,24 +9,12 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default class CustomApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     let { Component, pageProps } = this.props;
 
     return (
       <Layout>
-        <Container>
-          <Component {...pageProps} />
-        </Container>
+        <Component {...pageProps} />
       </Layout>
     );
   }
